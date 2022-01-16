f2b8c5dfb11397b48e20010f0d7fa6fdd6dab4f5
Erika Flores <foglzerika@gmail.com>
2022-01-15 22:31:04 -0600

    feat | Iteration 1 and 2 done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Create your functions
+const areaSquare= (side) =>{
+    return side * side
+}//areaSquare
+const areaTriangle = (base, height) =>{
+    return (base * height) / 2
+}//areaTriangle
+const areaCircle = (radius) =>{
+    return Math.PI *  (Math.pow(radius,2))
+}//areaCircle
+
+const perimeterSquare= (side) =>{
+    return side * side * side * side
+}//perimeterSquare
+const perimeterTriangle = (side) =>{
+    return 3 * side
+}//perimeterTriangle
+const perimeterCircle = (radius) =>{
+    return 2 * Math.PI * radius
+}//perimeterCircle
+
