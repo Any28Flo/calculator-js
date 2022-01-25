c777fb8856f6d960377fd15983dbf43180a3f201
Erika Flores <foglzerika@gmail.com>
2022-01-25 12:28:04 -0600

    feat | Add salarie function

---
+++ js/salarios.js
@@ -0,0 +1,13 @@
+
+const calculteSalaries = (array) =>{
+    let salarios =array.map(elemento =>{
+        return elemento.salary;
+    })
+
+}//
+window.onload = () =>{
+
+    console.log(1)
+    console.log(mexico);
+    //calculteSalaries()
+}
---
+++ js/salariosAnalisis.js
@@ -0,0 +1,62 @@
+let mexico = [];
+
+mexico.push({
+    name: "Migue",
+    salary : 800
+});
+
+mexico.push({
+    name: "Belén",
+    salary : 600
+});
+
+mexico.push({
+    name: "Luis",
+    salary : 20
+});
+
+mexico.push({
+    name: "Tamara",
+    salary : 400
+});
+
+mexico.push({
+    name: "Madona",
+    salary : 2000
+});
+
+mexico.push({
+    name: "Alberto",
+    salary : 500
+});
+
+mexico.push({
+    name: "Monica",
+    salary : 200
+});
+
+mexico.push({
+    name: "Pablo",
+    salary : 860
+});
+
+mexico.push({
+    name: "Sofia",
+    salary : 1500
+});
+
+mexico.push({
+    name: "Luis",
+    salary : 50
+});
+
+mexico.push({
+    name: "Ana",
+    salary : 800
+});
+
+
+mexico.push({
+    name: "Erika",
+    salary : 500
+});
